e303556641654b56b05558cba4d239de65d0ef35
Montana Flynn <montana949@gmail.com>
2025-09-23 13:58:15 -0500

    chore: remove unnecessary webpack config to fix Turbopack warning
    - Remove webpack ignore-loader config since test files are now outside pages/
    - Eliminates Turbopack/webpack compatibility warning in builds
    - Build is now clean with no warnings and works perfectly with Turbopack

--- next.config.js
+++ next.config.js
@@ -12,17 +12,6 @@ const nextConfig = {
       },
     ];
   },
-  
-  // Exclude test files from being treated as pages
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Exclude test files from being processed as pages
-    config.module.rules.push({
-      test: /\.(test|spec)\.(ts|tsx|js|jsx)$/,
-      use: 'ignore-loader',
-    });
-    
-    return config;
-  },
 };
 
 module.exports = nextConfig;
